9aed2188a0511d1e98c410a4df9aa3cb826ae92e
Chris Pesar <pesarcomputer@Chriss-MacBook-Air.local>
2021-10-19 20:47:57 -0600

    user marker added to map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,11 @@
-
+import { User } from './User';
+import { Company } from './Company';
 
 export class CustomMap{
   private googleMap: google.maps.Map;
 
-  constructor(){
-    this.googleMap = new google.maps.Map(document.getElementById('map'),{
+  constructor(divId: string){
+    this.googleMap = new google.maps.Map(document.getElementById(divId),{
       zoom: 1,
       center: {
         lat: 0,
@@ -12,4 +13,17 @@ export class CustomMap{
       }
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    })
+  }
+
+
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
-
 import { CustomMap } from "./CustomMap";
 
-new CustomMap();
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
 
 
 
